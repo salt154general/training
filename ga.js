723c194d04516cafb6f5be2c665271d776f1bd1a
salt154general <salt154general@gmail.com>
2024-10-06 21:24:42 +0900

    Update ga.js

--- ga.js
+++ ga.js
@@ -17,3 +17,32 @@ function sendPageView() {
         'member_status': isLoggedIn ? '会員' : '非会員'
     });
 }
+
+// ページロード時にページビュー送信
+window.onload = function () {
+    sendPageView();
+};
+
+// 購入リンクのクリックイベントをトラッキング
+document.querySelectorAll('.button a').forEach(function (element, index) {
+    element.addEventListener('click', function () {
+        // 商品情報を取得
+        const productContainer = element.closest('.product-container');
+        const productName = productContainer.querySelector('.product-info p:nth-child(1)').innerText.split(": ")[1];
+        const productCategory = productContainer.querySelector('.product-info p:nth-child(2)').innerText.split(": ")[1];
+        const productPrice = productContainer.querySelector('.product-info p:nth-child(3)').innerText.split(": ")[1].replace('円', '');
+
+        // purchaseイベントを送信
+        gtag('event', 'purchase', {
+            'transaction_id': 'TRX-' + index,  // 例: トランザクションID
+            'value': productPrice,  // 商品価格
+            'currency': 'JPY',  // 通貨
+            'items': [{
+                'item_name': productName,
+                'item_category': productCategory,
+                'price': productPrice,
+                'quantity': 1
+            }]
+        });
+    });
+});
